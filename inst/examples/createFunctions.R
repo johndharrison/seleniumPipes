23b8c9d55a331b383a5227d121c9e9079611fd8a
johndharrison <johndharrison0@gmail.com>
2016-07-31 15:05:46 -0700

    Update createFunctions to condition on request VERB

--- inst/examples/createFunctions.R
+++ inst/examples/createFunctions.R
@@ -30,16 +30,31 @@ funcTemp <- "#' {{command}}
 #' @examples
 
 {{command}} <- function({{Arg}}, ...){
+  {{{JSON_command}}}
   pathTemplate <- whisker.render(\"{{uriTemplate}}\", data = {{Arg}})
   pathURL <- {{Arg}}[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  queryDriver(verb = {{method}}, url = build_url(pathURL), ...)
+  queryDriver(verb = {{method}}, url = build_url(pathURL), json = {{JSON}},...)
 }
 
 
 "
 
-selPipeFuncs <- lapply(rowSplit(methPaths), function(x) whisker.render(funcTemp, x))
+selPipeFuncs <- lapply(rowSplit(methPaths), function(x){
+  if(identical(x[["method"]], "POST")){
+    x[["JSON_command"]] <- "
+# Add function specific JSON to post
+  jsonBody <- toJSON(list(
+
+  ), auto_unbox = TRUE)
+"
+    x[["JSON"]] <- "jsonBody"
+  }else{
+    x[["JSON"]] <- "NULL"
+  }
+  whisker.render(funcTemp, x)
+}
+)
 remDrFuncs <- paste(selPipeFuncs[!grepl("\\{elementId\\}", selPipeFuncs)], collapse = "")
 webElemFuncs <- paste(selPipeFuncs[grepl("\\{elementId\\}", selPipeFuncs)], collapse = "")
 write(remDrFuncs, "R/remoteDriver.R")
