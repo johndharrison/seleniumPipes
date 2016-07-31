a9327e7ebdacdd5974b1c6e0dd4fafcad3f0241a
johndharrison <johndharrison0@gmail.com>
2016-07-31 15:02:59 -0700

    Add zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onLoad <- function(libname = find.package("seleniumpipes"), pkgname = "seleniumpipes"){
+  op <- options()
+  op.seleniumPipes <- list(
+  )
+  toset <- !(names(op.seleniumPipes) %in% names(op))
+  if(any(toset)) options(op.seleniumPipes[toset])
+
+  invisible()
+}
