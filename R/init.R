80c8a2bbf5896442ac372d232701ad1206ae88fc
johndharrison <johndharrison0@gmail.com>
2016-07-31 15:03:29 -0700

    Ammend init.R

--- R/init.R
+++ R/init.R
@@ -6,17 +6,19 @@
 #' @examples
 #'
 
-remoteDr <- function(remoteServerAddr = "localhost",
+remoteDr <- function(remoteServerAddr = "http://localhost",
                      port             = 4444L,
                      browserName      = "firefox",
                      version          = "",
                      platform         = "ANY",
                      javascript       = TRUE,
                      nativeEvents     = TRUE,
-                     extraCapabilities = list()
+                     extraCapabilities = list(),
+                     path = "wd/hub"
                      , newSession = TRUE){
   remServAdd <- parse_url(remoteServerAddr)
   remServAdd[["port"]] <- port
+  remServAdd[["path"]] <- path
   session <- list(
     remServAdd = remServAdd,
     desiredCapabilities = list(
@@ -47,5 +49,5 @@ remoteDr <- function(remoteServerAddr = "localhost",
 #'
 
 queryDriver <- function(verb = GET, url, ...){
-  do.call(verb, c(list(url), as.list(...)))
+  res <- do.call(verb, c(list(url), body = list(...)[["json"]]))
 }
