fa7492819f5c1dcc1570a6b0647d55d85b9b7bf6
johndharrison <johndharrison0@gmail.com>
2016-08-01 07:00:58 -0700

    Add a switchToFrame example.

---
+++ inst/examples/switchToFrame.R
@@ -0,0 +1,21 @@
+remDr <- remoteDr()
+remDr %>% go("https://cran.r-project.org/")
+remDr %>% getTitle()
+frames <- remDr %>%  getPageSource() %>% xml_find_all("//frame") %>% xml_attr("name")
+
+# switch to main body
+remDr %>% switchToFrame(frames[3])
+remDr %>%  getPageSource() %>% xml_find_all("//a")
+remDr %>%  getPageSource() %>% xml_find_all("//a") %>% length
+#[1] 17
+
+# switch back to main frame
+
+remDr %>% switchToFrame()
+remDr %>%  getPageSource() %>% html_structure()
+
+# switch to side bar
+remDr %>% switchToFrame(frames[2])
+remDr %>%  getPageSource() %>% xml_find_all("//a")
+remDr %>%  getPageSource() %>% xml_find_all("//a") %>% length
+# [1] 13
