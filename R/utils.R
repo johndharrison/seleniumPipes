5f03d54ccc661e5239257ad6c5fcdd3899383018
johndharrison <johndharrison0@gmail.com>
2016-08-01 08:22:34 -0700

    Add print method for wElement

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,10 @@ print.rDriver <- function(x, ...){
   cat("Remote Ip Address:", build_url(x$remServAdd), "\n")
   cat("Remote sessionId:", x$sessionId(), "\n")
 }
+
+#' @export
+print.wElement <- function(x, ...){
+  cat("ElementId:", x$elementId[["ELEMENT"]], "\n")
+  cat("Remote Driver:", "\n")
+  print(x$remDr)
+}
