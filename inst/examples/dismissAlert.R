89da66300297a530d265f8eea5c6596818840e55
johndharrison <johndharrison0@gmail.com>
2016-08-02 11:41:14 -0700

    Add alerts examples.

---
+++ inst/examples/dismissAlert.R
@@ -0,0 +1,23 @@
+remDr <- remoteDr()
+remDr %>% go("https://www.google.com/ncr")
+remDr %>% getTitle()
+sScript <- "setTimeout(function(){alert('HELLO')},1000); return 'DONE';"
+remDr %>% executeScript(sScript, args = list())
+remDr %>% dismissAlert()
+
+sScript <- "setTimeout(function(){confirm('Press a button')},1000); return 'DONE';"
+remDr %>% executeScript(sScript, args = list())
+remDr %>% acceptAlert()
+
+sScript <- "setTimeout(function(){confirm('Press a button')},1000); return 'DONE';"
+remDr %>% executeScript(sScript, args = list())
+remDr %>% getAlertText()
+remDr %>% dismissAlert()
+
+
+sScript <- "setTimeout(function(){prompt('Please enter your name', 'Harry Potter')},1000); return 'DONE';"
+remDr %>% executeScript(sScript, args = list())
+remDr %>% getAlertText()
+remDr %>% sendAlertText("Buck Rogers?")
+
+remDr %>% deleteSession()
