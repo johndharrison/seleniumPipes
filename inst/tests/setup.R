c97246bbc44e3e29f90dc7eefa32ab435f84bba7
johndharrison <johndharrison0@gmail.com>
2016-08-13 06:41:49 -0700

    Update test setup.R

--- inst/tests/setup.R
+++ inst/tests/setup.R
@@ -5,9 +5,17 @@ if(is.null(testEnv$selOptions)){
   stop("Please set test options")
 }
 remDr <- do.call(remoteDr, testEnv$selOptions)
-remDr %>% setTimeout(3000) # set page load timeout to 3 secs
+remDr %>% setTimeout(milliseconds = 3000) # set page load timeout to 3 secs
 rdBrowser <- remDr$sessionInfo$browserName
-htmlSrc <- "localhost:3000"
+if(identical(testEnv$SL, TRUE)){
+  testEnv$SauceID <- remDr$sessionInfo$id
+}
+if(rdBrowser %in% c("iPhone", "iPad", "safari")){
+  htmlSrc <- "localhost:3000"
+}else{
+  # add somethin like 127.0.0.1	myname.local to /etc/hosts or equivalent
+  htmlSrc <- "myname.local:3000"
+}
 loadPage <- function(pgStr){
   paste0("http://", file.path(htmlSrc, paste0(pgStr, ".html")))
 }
