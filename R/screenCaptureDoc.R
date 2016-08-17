720d88cf061e7cd12a8b9289c9fcb57e3815f966
johndharrison <johndharrison0@gmail.com>
2016-08-17 07:38:16 -0700

    Update Documentation

---
+++ R/screenCaptureDoc.R
@@ -0,0 +1,25 @@
+#' takeElementScreenshot
+#'
+#' @param webElem
+#'
+#' @family screenCapture functions
+#' @return
+#' @export
+#'
+#' @examples
+#' @name takeElementScreenshot
+NULL
+
+#' takeScreenshot
+#'
+#' @param remDr
+#'
+#' @family screenCapture functions
+#' @return
+#' @export
+#'
+#' @examples
+#' @name takeScreenshot
+NULL
+
+
