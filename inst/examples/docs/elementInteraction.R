a37fc332ef3ee8746e6946a17cdaef7c14e5cb01
johndharrison <johndharrison0@gmail.com>
2016-08-19 14:17:03 -0700

    Add elementInteractions examples

--- inst/examples/docs/elementInteraction.R
+++ inst/examples/docs/elementInteraction.R
@@ -1,1 +1,24 @@
+\dontrun{
+  # start a brower
+  remDr <- remoteDr()
+  remDr %>% go("http://www.google.com/ncr")
 
+  queryBox <- remDr %>% findElement("name", "q")
+
+  # send text to the query box
+  queryBox %>% elementSendKeys("Some ", "text")
+
+  # clear the query box
+  queryBox %>% elementClear
+
+  # get the search button
+  searchBtn <- remDr %>% findElement("name", "btnG")
+  # send text to query box
+  queryBox %>% elementSendKeys("R project")
+
+  # click the search button
+  searchBtn %>% elementClick
+
+  # close browser
+  remDr %>% deleteSession
+}
