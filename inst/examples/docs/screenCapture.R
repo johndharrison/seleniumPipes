3ef36a09998f9a6db44d8c9a123e874c31af4d14
johndharrison <johndharrison0@gmail.com>
2016-08-19 14:18:02 -0700

    Add screenCapture examples

--- inst/examples/docs/screenCapture.R
+++ inst/examples/docs/screenCapture.R
@@ -1,1 +1,10 @@
+\dontrun{
+  remDr <- remoteDr()
+  remDr %>% go("http://www.google.com/ncr")
+  remDr %>% takeScreenshot
+
+  # select the search box
+  searchElem <- remDr %>% findElement("name", "q")
+  #searchElem %>% takeElementScreenshot()
+}
 
