06c4fdf59b5bdbfc7644423d5b4207a2f1c530db
johndharrison <johndharrison0@gmail.com>
2016-08-19 07:01:45 -0700

    Document the elementInteraction group.

--- R/elementInteractionDoc.R
+++ R/elementInteractionDoc.R
@@ -1,8 +1,9 @@
-#' elementClear
+#' Clear an elements text value.
 #'
-#'\code{elementClear}
+#'\code{elementClear} Clear a TEXTAREA or text INPUT element's value.
 #'
 #' @template webElem
+#' @template ellipsis
 #'
 #' @family elementInteraction functions
 #' @template ret7
@@ -12,11 +13,12 @@
 #' @name elementClear
 NULL
 
-#' elementClick
+#' Click on an element.
 #'
-#'\code{elementClick}
+#'\code{elementClick} The elementClick function scrolls into view the element and clicks the in-view centre point. If the element is not pointer-interactable, an element not interactable error is returned.
 #'
 #' @template webElem
+#' @template ellipsis
 #'
 #' @family elementInteraction functions
 #' @template ret7
@@ -26,11 +28,12 @@ NULL
 #' @name elementClick
 NULL
 
-#' elementSendKeys
+#' Send a sequence of key strokes to an element.
 #'
-#'\code{elementSendKeys}
+#'\code{elementSendKeys} The elementSendKeys function scrolls into view the form control element and then sends the provided keys to the element. In case the element is not keyboard interactable, an element not interactable error is returned.
 #'
 #' @template webElem
+#' @param ... keys to send the element. \code{seleniumPipes} includes mappings to unicode keys see \code{\link{selKeys}}. To use one of this name the string using key. See examples
 #'
 #' @family elementInteraction functions
 #' @template ret7
--- man-roxygen/ret7.R
+++ man-roxygen/ret7.R
@@ -1,1 +1,3 @@
-#' @return ret7 : invisible(webElem)
+#' @return invisible(webElem): An object of class "wElement" is invisibly returned.
+#'    A webElement object see \code{\link{wbElement}}. This allows for chaining from this function to
+#'    other functions that take such an object as an argument. See examples for further details.
