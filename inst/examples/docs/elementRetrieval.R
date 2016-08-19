b7c52ba07596e02bd7906f791d49a1baaba73aef
johndharrison <johndharrison0@gmail.com>
2016-08-19 14:17:19 -0700

    Add elementRetrieval examples

--- inst/examples/docs/elementRetrieval.R
+++ inst/examples/docs/elementRetrieval.R
@@ -1,1 +1,29 @@
+\dontrun{
+  remDr <- remoteDr()
+  remDr %>% go("http://www.google.com/ncr")
+
+  # find the search form query box and search for "R project"
+  webElem <- remDr %>% findElement("name", "q") %>%
+    elementSendKeys("R project", key = "enter")
+  # click the first link hopefully should be www.r-project.org
+  remDr %>% findElement("css", "h3.r a") %>% elementClick
+
+  # get the navigation div
+  navElem <- remDr %>% findElement("css", "div[role='navigation']")
+
+  # find all the links in this div
+  navLinks <- navElem %>% findElementsFromElement("css", "a")
+
+  # check the links
+  nLinks <- sapply(navLinks, function(x) x %>% getElementText)
+
+  # compare with all links
+  allLinks <- remDr %>% findElements("css", "a")
+  aLinks <- sapply(allLinks, function(x) x %>% getElementText)
+
+  # show the effect of searching for elements from element
+  aLinks %in% nLinks
+
+  remDr %>% deleteSession
+}
 
