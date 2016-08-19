3d2469e49ef5c9232dd4cc696049df8b196b66a6
johndharrison <johndharrison0@gmail.com>
2016-08-19 14:17:48 -0700

    Add navigation examples

--- inst/examples/docs/navigation.R
+++ inst/examples/docs/navigation.R
@@ -1,1 +1,26 @@
+\dontrun{
+  remDr <- remoteDr()
+  remDr %>% go("http://www.google.com/ncr")
+
+  # get the title
+  remDr %>% getTitle
+
+  # get the current page url
+  remDr %>% getCurrentUrl
+
+  # navigate
+  remDr %>% go("http://www.bbc.co.uk")
+
+  # go back
+  remDr %>% (seleniumPipes::back)
+
+  # go forward
+  remDr %>% forward
+
+  # refresh page
+  remDr %>% refresh
+
+  # close browser
+  remDr %>% deleteSession
+}
 
