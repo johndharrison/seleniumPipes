9d7152e6bd487f376bfc75f634e7ea234780eba1
johndharrison <johndharrison0@gmail.com>
2016-08-19 14:18:15 -0700

    Add sessions examples

--- inst/examples/docs/sessions.R
+++ inst/examples/docs/sessions.R
@@ -1,1 +1,15 @@
+\dontrun{
+  # start a driver without opening a browser
+  remDr <- remoteDr(newSession = FALSE)
+
+  # open a broswer
+  remDr %>% newSession
+
+  # set timeout on waiting for elements
+  remDr %>% setTimeout(type = "implicit", 5000)
+
+  # close Session
+  remDr %>% deleteSession
+
+}
 
