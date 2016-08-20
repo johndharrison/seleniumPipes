9448ce50c50c423cda5c27be867e9bfda7c1cafd
johndharrison <johndharrison0@gmail.com>
2016-08-20 16:47:24 -0700

    Experiment with retry option.

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,10 @@
     seleniumPipes_SL = FALSE,
     seleniumPipes_selOptions = list(),
     seleniumPipes_sauceID = "",
-    seleniumPipes_display_screenshot = TRUE
+    seleniumPipes_display_screenshot = TRUE,
+    seleniumPipes_retry = 3,
+    seleniumPipes_retry_delay = 5000
+
   )
   toset <- !(names(op.seleniumPipes) %in% names(op))
   if(any(toset)) options(op.seleniumPipes[toset])
