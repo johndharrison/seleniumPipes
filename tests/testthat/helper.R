b99af4d09b67165c83915a132fa8e4e6b5dcca3c
johndharrison <johndharrison0@gmail.com>
2016-09-02 19:40:26 -0700

    Move helper to function

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,28 +1,30 @@
-if(identical(TRUE, getOption("seleniumPipes_SL"))){
-  # sauce labs test
-  pv <- packageVersion("seleniumPipes")
-  slFlags <- list(name = "seleniumPipes-test-suite"
-                  , build = sum(unlist(pv)*10^(3-seq_along(unlist(pv)))) # 1.2.1 for example 121
-                  , tags =  list("api-example")
-                  , "custom-data" = list(release = do.call(paste, list(pv, collapse = ".")))
-  )
-  selOptions <- getOption("seleniumPipes_selOptions")
-  selOptions$extraCapabilities <- c(selOptions$extraCapabilities, slFlags)
-  options(seleniumPipes_selOptions = selOptions)
-  source(file.path("tests", "testthat", 'setup.R'), local = TRUE)
-}else{
-  remDr <- remoteDr(browserName = "chrome")
-  remDr %>% setTimeout(milliseconds = 10000) # set page load timeout to 3 secs
-  remDr %>% setTimeout(type = "implicit", milliseconds = 5000) # wait 5 secs for elements to load
-  htmlSrc <- if(identical(Sys.getenv("TRAVIS"), "true")){
-    "http-server:8080"
+initFun <- function(){
+  if(identical(TRUE, getOption("seleniumPipes_SL"))){
+    # sauce labs test
+    pv <- packageVersion("seleniumPipes")
+    slFlags <- list(name = "seleniumPipes-test-suite"
+                    , build = sum(unlist(pv)*10^(3-seq_along(unlist(pv)))) # 1.2.1 for example 121
+                    , tags =  list("api-example")
+                    , "custom-data" = list(release = do.call(paste, list(pv, collapse = ".")))
+    )
+    selOptions <- getOption("seleniumPipes_selOptions")
+    selOptions$extraCapabilities <- c(selOptions$extraCapabilities, slFlags)
+    options(seleniumPipes_selOptions = selOptions)
+    source(file.path("tests", "testthat", 'setup.R'), local = TRUE)
   }else{
-    "localhost:3000"
-  }
-  loadPage <- function(pgStr){
-    paste0("http://", file.path(htmlSrc, paste0(pgStr, ".html")))
-  }
-  rdBrowser <- remDr$sessionInfo$browserName
+    remDr <- remoteDr(browserName = "chrome")
+    remDr %>% setTimeout(milliseconds = 10000) # set page load timeout to 3 secs
+    remDr %>% setTimeout(type = "implicit", milliseconds = 5000) # wait 5 secs for elements to load
+    htmlSrc <- if(identical(Sys.getenv("TRAVIS"), "true")){
+      "http-server:8080"
+    }else{
+      "localhost:3000"
+    }
+    loadPage <- function(pgStr){
+      paste0("http://", file.path(htmlSrc, paste0(pgStr, ".html")))
+    }
+    rdBrowser <- remDr$sessionInfo$browserName
 
+  }
+  list(remDr = remDr, rdBrowser = rdBrowser)
 }
-
