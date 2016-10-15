c6ad483c75ef1caeb4f8f1de69bee23312ddc77b
johndharrison <johndharrison0@gmail.com>
2016-10-15 07:43:30 -0700

    Clean up element retrieval functions and documents.

--- R/elementRetrieval.R
+++ R/elementRetrieval.R
@@ -1,77 +1,110 @@
 
 #' @rdname findElement
-findElement <- function(remDr, using = c("xpath", "css selector", "id", "name", "tag name", "class name", "link text", "partial link text"), value, ...){
+findElement <- function(remDr, using = c("xpath", "css selector", "id",
+                                         "name", "tag name", "class name",
+                                         "link text", "partial link text"),
+                        value, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-# Add function specific JSON to post
+
+  # Add function specific JSON to post
   using <- match.arg(using)
   jsonBody <- toJSON(list(
     using = using, value = value
   ), auto_unbox = TRUE)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}/element", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/element", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "findElement", drvID = remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "findElement",
+                     drvID = remDr$drvID, json = jsonBody,...)
   invisible(wbElement(res$value, remDr))
 }
 
 #' @rdname findElementFromElement
-findElementFromElement <- function(webElem, using = c("xpath", "css selector", "id", "name", "tag name", "class name", "link text", "partial link text"), value, ...){
+findElementFromElement <- function(webElem,
+                                   using = c("xpath", "css selector", "id",
+                                             "name", "tag name",
+                                             "class name", "link text",
+                                             "partial link text"),
+                                   value, ...){
   obj <- webElem
   obj$sessionId <- webElem$sessionId(webElem$remDr$drvID)
   obj$elementId <- webElem$elementId$ELEMENT
-  
-# Add function specific JSON to post
+
+  # Add function specific JSON to post
   using <- match.arg(using)
   jsonBody <- toJSON(list(
     using = using, value = value
   ), auto_unbox = TRUE)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}/element/{{elementId}}/element", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/element/{{elementId}}/element",
+                   data = obj)
   pathURL <- webElem[['remDr']][['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "findElementFromElement", drvID = webElem$remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "findElementFromElement",
+                     drvID = webElem$remDr$drvID,
+                     json = jsonBody,...)
   invisible(wbElement(res$value, webElem$remDr))
 }
 
 
 
 #' @rdname findElements
-findElements <- function(remDr, using = c("xpath", "css selector", "id", "name", "tag name", "class name", "link text", "partial link text"), value, ...){
+findElements <- function(remDr, using = c("xpath", "css selector", "id",
+                                          "name", "tag name", "class name",
+                                          "link text",
+                                          "partial link text"),
+                         value, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-# Add function specific JSON to post
+
+  # Add function specific JSON to post
   using <- match.arg(using)
   jsonBody <- toJSON(list(
     using = using, value = value
   ), auto_unbox = TRUE)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}/elements", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/elements", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "findElements", drvID = remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "findElements",
+                     drvID = remDr$drvID, json = jsonBody,...)
   invisible(lapply(res$value, wbElement, remDr = remDr))
 }
 
 #' @rdname findElementsFromElement
-findElementsFromElement <- function(webElem, using = c("xpath", "css selector", "id", "name", "tag name", "class name", "link text", "partial link text"), value, ...){
+findElementsFromElement <- function(webElem,
+                                    using = c("xpath", "css selector",
+                                              "id", "name", "tag name",
+                                              "class name", "link text",
+                                              "partial link text"),
+                                    value, ...){
   obj <- webElem
   obj$sessionId <- webElem$sessionId(webElem$remDr$drvID)
   obj$elementId <- webElem$elementId$ELEMENT
-  
-# Add function specific JSON to post
+
+  # Add function specific JSON to post
   using <- match.arg(using)
   jsonBody <- toJSON(list(
     using = using, value = value
   ), auto_unbox = TRUE)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}/element/{{elementId}}/elements", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/element/{{elementId}}/elements",
+                   data = obj)
   pathURL <- webElem[['remDr']][['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "findElementsFromElement", drvID = webElem$remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "findElementsFromElement",
+                     drvID = webElem$remDr$drvID,
+                     json = jsonBody,...)
   invisible(lapply(res$value, wbElement, remDr = webElem$remDr))
 }
 
@@ -81,11 +114,14 @@ findElementsFromElement <- function(webElem, using = c("xpath", "css selector", 
 getActiveElement <- function(remDr, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}/element/active", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/element/active", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = GET, url = build_url(pathURL), source = "getActiveElement", drvID = remDr$drvID, json = NULL,...)
+  res <- queryDriver(verb = GET, url = build_url(pathURL),
+                     source = "getActiveElement",
+                     drvID = remDr$drvID, json = NULL,...)
   invisible(wbElement(res$value, remDr))
 }
 
--- R/elementRetrievalDoc.R
+++ R/elementRetrievalDoc.R
@@ -1,9 +1,14 @@
 #' Search for an element on the page
 #'
-#'\code{findElement} Search for an element on the page, starting from the document root. The located element will be returned as an object of "wElement" class
+#'\code{findElement} Search for an element on the page, starting from the
+#'    document root. The located element will be returned as an object of
+#'    "wElement" class
 #'
 #' @template remDr
-#' @param using Locator scheme to use to search the element, available schemes: {"class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath" }. Defaults to 'xpath'. Partial string matching is accepted.
+#' @param using Locator scheme to use to search the element, available
+#'    schemes: {"class name", "css selector", "id", "name", "link text",
+#'    "partial link text", "tag name", "xpath" }. Defaults to 'xpath'.
+#'    Partial string matching is accepted.
 #' @param value The search target. See examples.
 #' @template ellipsis
 #'
@@ -19,10 +24,15 @@ NULL
 
 #' Search for an element on the page, starting from another element
 #'
-#'\code{findElementFromElement} Search for an element on the page, starting from the node defined by the parent webElement. The located element will be returned as an object of wElement class.
+#'\code{findElementFromElement} Search for an element on the page,
+#'    starting from the node defined by the parent webElement. The located
+#'    element will be returned as an object of wElement class.
 #'
 #' @template webElem
-#' @param using Locator scheme to use to search the element, available schemes: {"class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath" }. Defaults to 'xpath'. Partial string matching is accepted.
+#' @param using Locator scheme to use to search the element, available
+#'    schemes: {"class name", "css selector", "id", "name", "link text",
+#'    "partial link text", "tag name", "xpath" }. Defaults to 'xpath'.
+#'    Partial string matching is accepted.
 #' @param value The search target. See examples.
 #' @template ellipsis
 #'
@@ -38,10 +48,15 @@ NULL
 
 #' Search for multiple elements on the page
 #'
-#'\code{findElements} Search for multiple elements on the page, starting from the document root. The located elements will be returned as a list of objects of class wElement.
+#'\code{findElements} Search for multiple elements on the page, starting
+#'    from the document root. The located elements will be returned as a
+#'    list of objects of class wElement.
 #'
 #' @template remDr
-#' @param using Locator scheme to use to search the element, available schemes: {"class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath" }. Defaults to 'xpath'. Partial string matching is accepted.
+#' @param using Locator scheme to use to search the element, available
+#'    schemes: {"class name", "css selector", "id", "name", "link text",
+#'    "partial link text", "tag name", "xpath" }. Defaults to 'xpath'.
+#'    Partial string matching is accepted.
 #' @param value The search target. See examples.
 #' @template ellipsis
 #'
@@ -57,10 +72,15 @@ NULL
 
 #' Search for multiple elements on the page, starting from another element.
 #'
-#'\code{findElementsFromElement} Search for multiple elements on the page, starting from the node defined by the parent webElement. The located elements will be returned as an list of objects of class wElement.
+#'\code{findElementsFromElement} Search for multiple elements on the page,
+#'    starting from the node defined by the parent webElement. The located
+#'    elements will be returned as an list of objects of class wElement.
 #'
 #' @template webElem
-#' @param using Locator scheme to use to search the element, available schemes: {"class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath" }. Defaults to 'xpath'. Partial string matching is accepted.
+#' @param using Locator scheme to use to search the element, available
+#'    schemes: {"class name", "css selector", "id", "name", "link text",
+#'    "partial link text", "tag name", "xpath" }. Defaults to 'xpath'.
+#'    Partial string matching is accepted.
 #' @param value The search target. See examples.
 #' @template ellipsis
 #'
@@ -76,7 +96,8 @@ NULL
 
 #' Get the element on the page that currently has focus.
 #'
-#'\code{getActiveElement} Get the element on the page that currently has focus. The located element will be returned as a WebcElement object.
+#'\code{getActiveElement} Get the element on the page that currently has
+#'    focus. The located element will be returned as a WebcElement object.
 #'
 #' @template remDr
 #' @template ellipsis
