65d21ddc2a2aed239e46774d42f20cf1260ec120
johndharrison <johndharrison0@gmail.com>
2016-10-15 07:26:49 -0700

    Clean up document handling functions and documents.

--- R/documentHandling.R
+++ R/documentHandling.R
@@ -1,9 +1,10 @@
 
 #' @rdname executeAsyncScript
-executeAsyncScript <- function(remDr, script, args = list(), replace = TRUE, ...){
+executeAsyncScript <- function(remDr, script, args = list(),
+                               replace = TRUE, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
+
   args <- lapply(args, function(x){
     if('wElement' %in% class(x)){
       x$elementId
@@ -15,18 +16,22 @@ executeAsyncScript <- function(remDr, script, args = list(), replace = TRUE, ...
     script = script, args = args
   ), auto_unbox = TRUE)
 
-  pathTemplate <- whisker.render("/session/{{sessionId}}/execute/async", data = obj)
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/execute/async", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "executeAsyncScript", drvID = remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "executeAsyncScript",
+                     drvID = remDr$drvID, json = jsonBody,...)
   if(replace){testWebElement(res$value, remDr)}else{res$value}
 }
 
 #' @rdname executeScript
-executeScript <- function(remDr, script, args = list(), replace = TRUE, ...){
+executeScript <- function(remDr, script, args = list(),
+                          replace = TRUE, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
+
   args <- lapply(args, function(x){
     if('wElement' %in% class(x)){
       x$elementId
@@ -38,10 +43,13 @@ executeScript <- function(remDr, script, args = list(), replace = TRUE, ...){
     script = script, args = args
   ), auto_unbox = TRUE)
 
-  pathTemplate <- whisker.render("/session/{{sessionId}}/execute/sync", data = obj)
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/execute/sync", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "executeScript", drvID = remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "executeScript",
+                     drvID = remDr$drvID, json = jsonBody,...)
   if(replace){testWebElement(res$value, remDr)}else{res$value}
 }
 
@@ -49,11 +57,14 @@ executeScript <- function(remDr, script, args = list(), replace = TRUE, ...){
 getPageSource <- function(remDr, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}/source", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/source", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = GET, url = build_url(pathURL), source = "getPageSource", drvID = remDr$drvID, json = NULL,...)
+  res <- queryDriver(verb = GET, url = build_url(pathURL),
+                     source = "getPageSource",
+                     drvID = remDr$drvID, json = NULL,...)
   read_html(res$value)
 }
 
--- R/documentHandlingDoc.R
+++ R/documentHandlingDoc.R
@@ -1,15 +1,17 @@
 #' Execute JavaScript asynchronously on browser.
 #'
-#'\code{executeAsyncScript} Inject a snippet of JavaScript into the page for asynchronous execution in
-#'    the context of the currently selected frame.
+#'\code{executeAsyncScript} Inject a snippet of JavaScript into the page
+#'    for asynchronous execution in the context of the currently selected
+#'    frame.
 #'
-#'    The driver will pass a callback as the last argument to the snippet, and
-#'    block until the callback is invoked.
+#'    The driver will pass a callback as the last argument to the snippet,
+#'    and block until the callback is invoked.
 #'
 #' @template remDr
 #' @param script character: The script to inject.
 #' @param args The arguments of the script as a list.
-#' @param replace logical: If TRUE any elements identify as web Elements are converted to such.
+#' @param replace logical: If TRUE any elements identify as web Elements
+#'    are converted to such.
 #' @template ellipsis
 #'
 #' @family documentHandling functions
@@ -22,14 +24,16 @@ NULL
 
 #' Execute JavaScript on browser.
 #'
-#'\code{executeScript}  Inject a snippet of JavaScript into the page for execution in the context
-#'    of the currently selected frame. The executed script is assumed to be
-#'    synchronous and the result of evaluating the script will be returned.
+#'\code{executeScript}  Inject a snippet of JavaScript into the page for
+#'    execution in the context of the currently selected frame. The
+#'    executed script is assumed to be synchronous and the result of
+#'    evaluating the script will be returned.
 #'
 #' @template remDr
 #' @param script character: The script to inject.
 #' @param args The arguments of the script as a list.
-#' @param replace logical: If TRUE any elements identify as web Elements are converted to such.
+#' @param replace logical: If TRUE any elements identify as web Elements
+#'    are converted to such.
 #' @template ellipsis
 #'
 #' @family documentHandling functions
