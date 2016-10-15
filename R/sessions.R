87d14d9b1346478d9cd3f9c932ef79723379f1af
johndharrison <johndharrison0@gmail.com>
2016-10-15 08:18:28 -0700

    Add status and getTimeouts methods to session group. Clean up session functions and documents.

--- R/sessions.R
+++ R/sessions.R
@@ -3,41 +3,85 @@
 deleteSession <- function(remDr, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-  pathTemplate <- whisker.render("/session/{{sessionId}}", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = DELETE, url = build_url(pathURL), source = "deleteSession", drvID = remDr$drvID, json = NULL,...)
+  res <- queryDriver(verb = DELETE, url = build_url(pathURL),
+                     source = "deleteSession",
+                     drvID = remDr$drvID, json = NULL,...)
   .e$sessionId[[remDr$drvID]] <- NULL;invisible(remDr)
+}
+
+#' @rdname getTimeouts
+getTimeouts <- function(remDr, type = "page load", ...){
+  obj <- remDr
+  obj$sessionId <- remDr$sessionId(remDr$drvID)
+
+  jsonBody <- toJSON(list(type = type),
+                     auto_unbox = TRUE)
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/timeouts", data = obj)
+  pathURL <- remDr[['remServAdd']]
+  pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
+  res <- queryDriver(verb = GET, url = build_url(pathURL),
+                     source = "getTimeouts",
+                     drvID = remDr$drvID, json = NULL,...)
+  res$value
 }
 
 #' @rdname newSession
 newSession <- function(remDr, ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-# Add function specific JSON to post
+
+  # Add function specific JSON to post
   jsonBody <- toJSON(list(
-    desiredCapabilities =c(remDr$desiredCapabilities, remDr$extraCapabilities)
+    desiredCapabilities =c(remDr$desiredCapabilities,
+                           remDr$extraCapabilities)
   ), auto_unbox = TRUE)
-  
-  pathTemplate <- whisker.render("/session", data = obj)
+
+  pathTemplate <-
+    whisker.render("/session", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "newSession", drvID = remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "newSession",
+                     drvID = remDr$drvID, json = jsonBody,...)
   remDr$sessionInfo <- res$value;invisible(remDr)
 }
 
-#' @rdname setTimeout
-setTimeout <- function(remDr, type = "page load", milliseconds = 10000, ...){
+#' @rdname setTimeouts
+setTimeouts <- function(remDr, type = "page load", milliseconds = 10000,
+                        ...){
   obj <- remDr
   obj$sessionId <- remDr$sessionId(remDr$drvID)
-  
-      jsonBody <- toJSON(list(type = type, ms = milliseconds), auto_unbox = TRUE)
-  pathTemplate <- whisker.render("/session/{{sessionId}}/timeouts", data = obj)
+
+  jsonBody <- toJSON(list(type = type, ms = milliseconds),
+                     auto_unbox = TRUE)
+  pathTemplate <-
+    whisker.render("/session/{{sessionId}}/timeouts", data = obj)
   pathURL <- remDr[['remServAdd']]
   pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
-  res <- queryDriver(verb = POST, url = build_url(pathURL), source = "setTimeout", drvID = remDr$drvID, json = jsonBody,...)
+  res <- queryDriver(verb = POST, url = build_url(pathURL),
+                     source = "setTimeouts",
+                     drvID = remDr$drvID, json = jsonBody,...)
   invisible(remDr)
+}
+
+#' @rdname status
+status <- function(remDr, ...){
+  obj <- remDr
+  obj$sessionId <- remDr$sessionId(remDr$drvID)
+
+  pathTemplate <-
+    whisker.render("/status", data = obj)
+  pathURL <- remDr[['remServAdd']]
+  pathURL[['path']] <- paste0(pathURL[['path']], pathTemplate)
+  res <- queryDriver(verb = GET, url = build_url(pathURL),
+                     source = "status",
+                     drvID = remDr$drvID, json = NULL,...)
+  res$value
 }
 
--- R/sessionsDoc.R
+++ R/sessionsDoc.R
@@ -15,7 +15,10 @@ NULL
 
 #' Create a new session.
 #'
-#'\code{newSession} The server should attempt to create a session that most closely matches the desired and required capabilities. Required capabilities have higher priority than desired capabilities and must be set for the session to be created.
+#'\code{newSession} The server should attempt to create a session that
+#'    most closely matches the desired and required capabilities. Required
+#'    capabilities have higher priority than desired capabilities and must
+#'    be set for the session to be created.
 #'
 #' @template remDr
 #' @template ellipsis
@@ -28,14 +31,40 @@ NULL
 #' @name newSession
 NULL
 
-#' Configure the amount of time that a particular type of operation can execute
+#' Get amount of time that a particular type of operation can execute in.
 #'
-#'\code{setTimeout} Configure the amount of time that a particular type of operation can execute
-#'    for before they are aborted and a |Timeout| error is returned to the client.
+#'\code{getTimeouts} The Get Timeout command gets timeouts associated with
+#'    the current session.
 #'
 #' @template remDr
-#' @param type The type of operation to set the timeout for. Valid values are: "script" for script timeouts, "implicit" for modifying the implicit wait timeout and "page load" for setting a page load timeout.
-#' @param milliseconds The amount of time, in milliseconds, that time-limited commands are permitted to run.
+#' @param type The type of operation to set the timeout for. Valid values
+#'    are: "script" for script timeouts, "implicit" for modifying the
+#'    implicit wait timeout and "page load" for setting a page load
+#'    timeout.
+#' @template ellipsis
+#'
+#' @family sessions functions
+#' @template ret2
+#' @export
+#'
+#' @example /inst/examples/docs/sessions.R
+#' @name getTimeouts
+NULL
+
+#' Configure the amount of time that a particular type of operation can
+#'    execute
+#'
+#'\code{getTimeouts} Configure the amount of time that a particular type
+#'    of operation can execute for before they are aborted and a |Timeout|
+#'    error is returned to the client.
+#'
+#' @template remDr
+#' @param type The type of operation to set the timeout for. Valid values
+#'    are: "script" for script timeouts, "implicit" for modifying the
+#'    implicit wait timeout and "page load" for setting a page load
+#'    timeout.
+#' @param milliseconds The amount of time, in milliseconds, that
+#'    time-limited commands are permitted to run.
 #' @template ellipsis
 #'
 #' @family sessions functions
@@ -43,7 +72,25 @@ NULL
 #' @export
 #'
 #' @example /inst/examples/docs/sessions.R
-#' @name setTimeout
+#' @name setTimeouts
 NULL
 
+#' Get remote end status.
+#'
+#'\code{status} The Status command returns information about whether a
+#'    remote end is in a state in which it can create a new session.
+#'    This is represented by the ready property of the response body,
+#'    which has a value of false if attempting to create a session at
+#'    the current time would fail.
+#'
+#' @template remDr
+#'
+#' @family sessions functions
+#' @template ret2
+#' @template ellipsis
+#' @export
+#'
+#' @example /inst/examples/docs/sessions.R
+#' @name status
+NULL
 
