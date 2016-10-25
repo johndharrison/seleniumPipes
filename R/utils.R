f36da41638c5017586c93c53f55c030a380b0874
johndharrison <johndharrison0@gmail.com>
2016-10-25 11:49:40 -0700

    Export pipe operator

--- R/utils.R
+++ R/utils.R
@@ -78,3 +78,15 @@ testWebElement <- function(x, remDr){
     replaceWE(x, remDr = remDr)
   }
 }
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
